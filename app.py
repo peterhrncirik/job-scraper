6b0d2b1a76276063d50e37eda4da9af970c1c175
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-12-04 20:43:11 +0100

    added text clearing function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 def extract(page):
     
@@ -48,24 +49,50 @@ def get_job_summary(link):
     url = f'https://www.profesia.sk{link}'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'html.parser')
-    summary_div = bool(soup.find('div', attrs={'class': 'job-info'}))
+    summary_div = bool(soup.find('div', attrs={'class': 'details'}))
     
     if summary_div:
-        return soup.find('div', attrs={'class': 'job-info'}).text.strip().replace('\n', '')
+        
+        # Get the whole page
+        page_details = soup.find('div', attrs={'class': 'details'})
+
+        # Remove h4 titles from the page, join text together and return it
+        return ' '.join(text for text in page_details.find_all(text=True) if text.parent.name != "h4")
+
     else:
         return ''
 
 # Initiate list to append each job into    
 jobs = []
+jobs_skills_matched = []
 
 # Get jobs from more pages
-for _ in range(1, 3):
-    p = extract(_)
+for i in range(1, 2):
+    print(f'Reading page #{i}...')
+    p = extract(i)
     transform(p)
 
 # Transform to DataFrame
 df = pd.DataFrame(jobs)
 df.to_csv('jobs.csv')
 
-
+def analyze_job(jobs):
     
+    for job in jobs:
+        
+        # clear job description for analysis
+        summary = job['summary'].strip().replace('\n', '').replace('-', ' ').replace('?', ' ').replace('!', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('(',' ').replace(')',' ').lower()
+
+        # create new list of all words from description
+        words = summary.split()
+
+        # check keywords in words
+        
+        
+        # if match, append job link to jobs_skills_matched
+
+
+def sent_email(jobs):
+    pass
+    
+analyze_job(jobs)
