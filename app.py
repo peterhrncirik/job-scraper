f87c0a9a651503fb8dbb995983e59914e283464b
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-12-05 13:12:53 +0100

    Implemented advanced skill analysis

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
 import time
+import random
+
+# For readability and progress bar
+from rich import print
+from rich.progress import track
+
+# Text analysis
+from rake_nltk import Rake
+
 
 def extract(page):
     
@@ -13,12 +23,19 @@ def extract(page):
 
 def transform(soup):
     
+    # Initialize Rake object
+    rake = Rake()
+
     # Get the whole jobs column
     divs = soup.find_all('li', class_ = 'list-row')
 
     # Iterate over each job div
-    for i, item in enumerate(divs):
+    for item in track(divs, description='Extracting job infos...'):
         
+        # Print progress bar
+        time.sleep(.3)
+
+
         # Skip the newsletter div
         if bool(item.find('form', class_=['form-horizontal'])):
             continue
@@ -28,14 +45,19 @@ def transform(soup):
         company = item.find('span', attrs={'class': 'employer'}).text.strip()
         salary = item.find('span', class_=['label-bordered', 'green']).text.strip()
         link = item.find('span', attrs={'class': 'title'}).parent.get('href')
+
         # Open each job and return job summary
         summary = get_job_summary(link)
-            
+        
+        # Analyze summary for keywords
+        rake.extract_keywords_from_text(summary)
+
         job = {
             'title': title,
             'company': company,
             'salary': salary,
-            'summary': summary,
+            # Append summary keywords as a set to remove duplicates
+            'summary': set(rake.get_ranked_phrases()),
             'link': f'https://www.profesia.sk{link}'
         }
         
@@ -49,53 +71,49 @@ def get_job_summary(link):
     url = f'https://www.profesia.sk{link}'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'html.parser')
-    summary_div = bool(soup.find('div', attrs={'class': 'details'}))
+    return soup.find('main', attrs={'id': 'detail'}).text
     
-    if summary_div:
-        
-        # Get the whole page
-        page_details = soup.find('div', attrs={'class': 'details'})
-
-        # Remove h4 titles from the page, join text together and return it
-        return ' '.join(text for text in page_details.find_all(text=True) if text.parent.name != "h4")
-
-    else:
-        return ''
+    
 
 # Initiate list to append each job into    
 jobs = []
 jobs_skills_matched = []
-SKILLS = ('django', 'python', 'sql', 'flask', 'javascript', 'junior')
+SKILLS = ('django', 'python', 'sql', 'flask', 'javascript', 'junior', 'css', 'html', 'html/css')
 
 # Get jobs from more pages
-for i in range(1, 2):
+for i in range(1, 3):
     print(f'Reading page #{i}...')
     p = extract(i)
     transform(p)
 
-# Transform to DataFrame
-df = pd.DataFrame(jobs)
-df.to_csv('jobs.csv')
+# Transform to DataFrame and export to csv
+# df = pd.DataFrame(jobs)
+# df.to_csv('jobs.csv')
 
 def analyze_job(jobs):
-    
-    for job in jobs:
-        
-        # clear job description for analysis
-        summary = job['summary'].strip().replace('\n', '').replace('-', ' ').replace('?', ' ').replace('!', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('(',' ').replace(')',' ').lower()
 
-        # create new list of all words from description
-        words = summary.split()
+    for job in track(jobs, description="Analyzing jobs..."):
 
-        # check keywords in job description
-        match = [i for i in SKILLS if i in words]
+        # Print progress bar
+        time.sleep(.3)
+
+        # check for keywords in summary as well in title
+        match = [skill for skill in SKILLS if skill in job['summary'] or skill in job['title'].lower()]
         
         # if match, append job link to jobs_skills_matched
         if match:
-            jobs_skills_matched.append(job['link'])
+            jobs_skills_matched.append({
+                'link': job['link'],
+                # the more skills matched, the higher score
+                'score': len(match), 
+                'skills': match,
+            })
+    return
+
 
 
 def sent_email(jobs):
     pass
     
 analyze_job(jobs)
+
