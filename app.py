2eaac13a8d6971087141c5386afbe41deca7cff0
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-12-11 08:44:04 +0100

    Implemented IDs checking

--- app.py
+++ app.py
@@ -24,7 +24,14 @@ args = parser.parse_args()
 # Initiate list to append each job into    
 jobs = []
 jobs_skills_matched = []
+jobs_already_seen = []
 SKILLS = ('django', 'django developer', 'django junior developer', 'python developer', 'python junior developer', 'python junior', 'python', 'sql', 'flask', 'javascript', 'junior', 'css', 'html', 'html/css')
+
+# load IDs of already seen jobs
+with open('ids.txt') as file:
+    
+    for id in file:
+        jobs_already_seen.append(id.strip())
 
 
 def extract(page):
@@ -44,10 +51,10 @@ def transform(soup):
     divs = soup.find_all('li', class_ = 'list-row')
 
     # Iterate over each job div
-    for item in track(divs[:3], description='Extracting job infos...'):
+    for item in track(divs, description='Extracting job infos...'):
         
         # Print progress bar
-        time.sleep(.3)
+        time.sleep(2)
 
 
         # Skip the newsletter div
@@ -60,6 +67,14 @@ def transform(soup):
         salary = item.find('span', class_=['label-bordered', 'green']).text.strip()
         link = item.find('span', attrs={'class': 'title'}).parent.get('href')
 
+        # Split URL for ID and save it to avoid sending same job postings
+        id = link.split('/')[-1]
+        
+        if id not in jobs_already_seen:
+            with open('ids.txt', 'a+') as file:
+
+                file.write(id + '\n')
+        
         # Open each job and return job summary
         summary = get_job_summary(link)
         
@@ -67,6 +82,7 @@ def transform(soup):
         rake.extract_keywords_from_text(summary)
 
         job = {
+            'id': id,
             'title': title,
             'company': company,
             'salary': salary,
@@ -103,19 +119,20 @@ def analyze_job(jobs):
     for job in track(jobs, description="Analyzing jobs..."):
 
         # Print progress bar
-        time.sleep(.3)
+        time.sleep(2)
 
         # check for keywords in summary as well in title
         match = [skill for skill in SKILLS if skill in job['summary'] or skill in job['title'].lower()]
-        
-        # if match, append job link to jobs_skills_matched
-        if match:
+                
+        # if match and not already seen, append job link to jobs_skills_matched
+        if match and job['id'] not in jobs_already_seen:
             jobs_skills_matched.append(f'Score: {len(match)} | Skills: {match} | {job["link"]}')
             
     return
+
     
 analyze_job(jobs)
 
-# send email
+# send email  
 notify(jobs=sorted(jobs_skills_matched, reverse=True))
 
